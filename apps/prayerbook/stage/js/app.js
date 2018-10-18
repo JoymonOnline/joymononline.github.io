8289df85a147954d9a914dd7673a446545edaa20
joymon <joymon@gmail.com>
2018-10-17 20:10:29 -0400

    Deployment
    Deployment

--- apps/prayerbook/stage/js/app.js
+++
@@ -1,4 +0,0 @@
-
-var app = angular.module('prayerBook', ['ui.bootstrap']);
-app.controller('MainCtrl', ['$scope', '$http', '$sce','$modal', MainCtrl]);
-app.controller('DevCreditsCtrl',  DevCreditsCtrl);
